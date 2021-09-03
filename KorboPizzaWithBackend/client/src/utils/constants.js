9e74180f9db6342368c7d39063a0cd5d887e68f6
Root Root <abrm38957@gmail.com>
2021-09-03 06:05:46 +0300

    React projects have been committed

--- KorboPizzaWithBackend/client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_ROUTE = '/login';
-export const REGISTRATION_ROUTE = '/registration';
-export const HOME_ROUTE = '/';
-export const CART_ROUTE = '/cart';
--- KorboPizzaWithBackend/server/routes/pizza.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const pizzaController = require('../controllers/pizza');
-
-router.get('/', pizzaController.getAll);
-
-module.exports = router;
